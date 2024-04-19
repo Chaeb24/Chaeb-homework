095c1fa7d91a83af09614a457b38c1431acf1cab
Chaeb <162852897+Chaeb24@users.noreply.github.com>
2024-04-19 17:36:02 +0900

    hw4.py

--- hw4.py
+++ hw4.py
@@ -1,18 +1,18 @@
 def rep_char(c,n):
     print(c*n)
 
-def draw_line_string(msg):
+def draw_line_string(name):
+    msg1='Hello'+ name + ','
+    msg2='Welcome to Seoul'
     nstr = len(msg1)if(len(msg1)>len(msg2))else len(msg2)
-    rep_char('-',nstr)
-    print(f'Hello {msg},')
-    print('Welcome to Seoul.')
-    rep_char('-',nstr)
+    rep_char('-',nstr+2)
+    print(f' {msg1} ')
+    print(f' {msg2} ')
+    rep_char('-',nstr+2)
 
-msg1 = input('Input his/her name:')
-msg2 = input('Input his/her name:')
+n = input('Input his/her name:')
+draw_line_string(n)
 
-draw_line_string(msg1)
-draw_line_string(msg2)
 
 
             
