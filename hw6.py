5ce168dadf7e6db9526b30341da782e6e5180b38
Chaeb <162852897+Chaeb24@users.noreply.github.com>
2024-04-26 09:16:04 +0900

    Add files via upload

---
+++ hw6.py
@@ -0,0 +1,18 @@
+for i in range(1, 10):
+    for j in range(2, 6):
+        print(j, "x", i, "=", i*j, end = '\t')
+    print("\n")
+print("\n")
+
+for i in range(1, 10):
+    for j in range(6, 10):
+        print(j, "x", i, "=", i*j, end = '\t')
+    print("\n")
+
+
+
+
+
+    
+    
+    
